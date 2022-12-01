62552b95d3acf298c5e443f28c4ed673faf6f7ab
Vitor Klein <vitor@kdatabi.com>
2022-11-30 23:29:33 -0300

    first commit

---
+++ src/main/resources/db/V1__Tables.sql
@@ -0,0 +1,2 @@
+create user katruadmin with password '';
+create database 
---
+++ src/test/java/com/katru/api/KatruApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katru.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatruApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
